b55a2b9672290182edb76de69a35ee629502269e
Ulisse Mini <ulisse.mini@gmail.com>
2019-04-18 12:55:06 -0400

    Commit failing test

--- coords/spec/moveTo_spec.lua
+++ coords/spec/moveTo_spec.lua
@@ -1,28 +1,28 @@
--- require('fs')
--- require('turtle')
--- require('textutils')
--- 
--- local coords = dofile('../coords.lua')
--- 
--- describe('turtle.moveTo', function()
---   it('should be able to travel away and back', function()
---     turtle.moveTo(1, 2, 3, 4)
--- 
---     assert.are.same(coords,
---       {
---         x = 1,
---         y = 2,
---         z = 3,
---         ori = 4,
---       })
--- 
---     turtle.moveTo(0, 0, 0, 0)
---     assert.are.same(coords,
---       {
---         x = 0,
---         y = 0,
---         z = 0,
---         ori = 0,
---       })
---   end)
--- end)
+require('fs')
+require('turtle')
+require('textutils')
+
+local coords = dofile('../coords.lua')
+
+describe('turtle.moveTo', function()
+  it('should be able to travel away and back', function()
+    turtle.moveTo(1, 2, 3, 4)
+
+    assert.are.same(coords,
+      {
+        x = 1,
+        y = 2,
+        z = 3,
+        ori = 4,
+      })
+
+    turtle.moveTo(0, 0, 0, 0)
+    assert.are.same(coords,
+      {
+        x = 0,
+        y = 0,
+        z = 0,
+        ori = 0,
+      })
+  end)
+end)
